05f65713381b29a535ccfecfb5554db97429573a
razcrisan <rcrisan13@gmail.com>
2017-10-05 15:13:44 -0400

    final commit

--- IntroToOOP/Cat.cs
+++ IntroToOOP/Cat.cs
@@ -8,5 +8,29 @@ namespace IntroToOOP
 {
     class Cat
     {
+        //STATES
+        //fields
+        private string name;
+        
+        
+        //Properties - half a field half a method(action word)
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+
+
+        //BEHAVIORS
+
+        //Constructors
+        public Cat()
+        {
+            // default constructor because it takes no
+            // parameters in the parenthesis
+
+        }
+
     }
 }
